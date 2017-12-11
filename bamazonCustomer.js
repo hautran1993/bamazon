3d675093dca90a4296d521d9471895fb13190083
Your Name <hautran1993@yahoo.com>
2017-12-11 14:57:27 -0600

    pusdo coding

---
+++ bamazonCustomer.js
@@ -0,0 +1,23 @@
+//create mysql database called bamazon
+//create a table inside of the database called products
+    //the products should have
+        //item_id(unique id for each product)
+        //product_name (name of product)
+        //deparment_name
+        // price (cost to customer)
+        //stock_quanity
+    //populate is database with around 10 different products
+    //(i.e insert 'mock' data rows int this data base and table).
+//create a file call bamazoncustomer.js
+    //running this application will first display all of the items
+    //includes the ids, names, produts, and prices of products for sale.
+//the app should prompt users with two messages.
+    //the first prompt should ask them for the id of the product they would like to purchase
+    //the second message should ask how many units of the products they would like to buy
+//once the customer has placed the rder, your applucatuon should check if your store has enough of the products for the customer's request.
+    //if not the app should log  phrase like 'insufficient quanity!'
+    //then prevent the order form going in
+//if the store does have enough product, you should fullfill the customer's oder.
+    //this means updating the sql database to reflecg the remaining quanity.
+    //once the updates goes through, show the customer the total cost of their purchse.
+            
